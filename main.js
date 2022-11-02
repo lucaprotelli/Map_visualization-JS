59e694add26c75b7b0b061e6521d99efa8bf0b08
Luca Protelli <lucaprotelli23@icloud.com>
2022-11-02 01:51:20 +0100

    Fixato tutti i bug del frontend!

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 import "./style.css";
 
+const map = L.map("map").setView([51.505, -0.09], 13);
+
+const osm = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
 fetch("/data.json")
   .then((response) => response.json())
   .then((json) => console.log(json));
-
-const map = L.map("map").setView([51.505, -0.09], 13);
-L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  maxZoom: 19,
-  attribution:
-    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-}).addTo(map);
